1f6f045ddfb7cd50306b531689afc6396ad7765d
Roro <bsrskumar.28@gmail.com>
2018-11-12 01:35:17 +0530

    Added new Variables. Added a new function for RayCasting to where the CrossHair is pointed. Function under implementation.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,6 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+#include "Public/CollisionQueryParams.h"
+#include "Math/Vector2D.h"
 
 void ATankPlayerController::BeginPlay()
 {
@@ -39,8 +43,30 @@ void ATankPlayerController::AimTowardsCrossHair()
 {
 	if (!GetControlledTank())
 		return;
+	
+	FVector HitLocation{ 0 }; // out parameter
 
-	// Get World Location if Line trace through crosshair.
-	// If it hits the landscape 
-		// Tell the control tank to aim at this point.
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		// If it hits the landscape 
+		//UE_LOG(LogTemp, Warning, TEXT("HitLocation %s"), *HitLocation.ToString());
+			// Tell the control tank to aim at this point.
+	}
+}
+
+// Get World Location if Line trace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector &HitLocation) const
+{
+
+	// Find the cross hair position
+	int32 ViewPortSizeX, ViewPortSizeY;
+
+	GetViewportSize(ViewPortSizeX, ViewPortSizeY);
+
+	FVector2D ScreenLocation(ViewPortSizeX * CrossHairXLocation, ViewPortSizeY*CrossHairYLocation);
+
+		// "De-Project" the screen position of the crosshair to a world direction
+		// LineTrace along that look direction, and see what we hit (up to a max range).
+
+		return true;
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -15,7 +15,7 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	virtual void BeginPlay() override;
 	
 	//Implement the tick function. Same as all the other tick functions.
@@ -23,10 +23,17 @@ public:
 
 	ATank* GetControlledTank() const;
 
-private:
 	// Start the tank moving the barrel so that a shot would hit where the crosshairs
 	// intersects the world.
 	void AimTowardsCrossHair();
+
+	bool GetSightRayHitLocation(FVector &HitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.333f;
 
 	
 };
