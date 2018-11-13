7e46a898303b64a6d28c9278f555fe4af7486413
Roro <bsrskumar.28@gmail.com>
2018-11-14 01:11:31 +0530

    Updated the UCLASS() Macro. It now shows up in the Unreal Editor under AddComponents for Blueprints. Category will be Custom. Added new variables.
    Changed the comments before UTankAimingComponent.
    
    Updated the Tank BP with the Barrel Class rather than Static Mesh Component.

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -9,13 +9,21 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 
 public:
 	void Elevate(float DegreesPerSecond);
+
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxDegreesPerSecond = 20.0f; // Sensible Default
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxElevationDegrees = 40.0;	
 	
-	
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MinElevationDegrees = 0.0;   
 };
